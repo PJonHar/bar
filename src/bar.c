4ffd4d0dd02923caab2350f0ac4017a68ee6050c
PJonHar <pauljonharrold@gmail.com>
2013-04-15 18:31:30 +0100

    Better Clock & Date Added

--- src/bar.c
+++ src/bar.c
@@ -2,68 +2,30 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
-
+#define SCREEN_WIDTH 144
+#define SCREEN_HEIGHT 168
+#define STATUS_BAR_HEIGHT 16
 #define MY_UUID { 0x95, 0xD4, 0x0E, 0xBE, 0xAD, 0x89, 0x44, 0x6F, 0x81, 0x68, 0x04, 0x0D, 0xB5, 0x02, 0x7D, 0x1C }
 PBL_APP_INFO(MY_UUID,
              "Bar Clock", "PJonHar",
-             1, 0, /* App version */
+             0, 3, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_WATCH_FACE);
-
+			 
 Window window;
 Layer bars_layer;
-TextLayer text_time_layer;
-static int iScreenWidth=144;
-static int iScreenHeight=168;
-//static int iStatusBarHeight=16;
+TextLayer text_hour_layer, text_min_layer, text_sec_layer, text_date_layer;
+int i, iBarWidth, iBarHeight, iBarStart;
+int iPointOne[2], iPointTwo[2], iPointThree[2], iPointFour;
+PblTm current_time;
 
-// Taken from Simplicity
-void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
-    (void)t;
-    (void)ctx;
-    
-    // Need to be static because they're used by the system later.
-    static char time_text[] = "00:00:00";
-    //static char date_text[] = "Xxxxxxxxx 00";
-    
-    char *time_format;
-    
-    // TODO: Only update the date when it's changed.
-    // string_format_time(date_text, sizeof(date_text), "%B %e", t->tick_time);
-    //text_layer_set_text(&text_date_layer, date_text);
-    
-    
-    if (clock_is_24h_style()) {
-        time_format = "%R";
-    } else {
-        time_format = "%I:%M:%I";
-    }
-    
-    string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
-    
-    // Kludge to handle lack of non-padded hour format string
-    // for twelve hour clock.
-    if (!clock_is_24h_style() && (time_text[0] == '0')) {
-        memmove(time_text, &time_text[1], sizeof(time_text) - 1);
-    }
-    
-    text_layer_set_text(&text_time_layer, time_text);
-    
-    layer_mark_dirty(&bars_layer);
-}
-
-void bars_layer_update_callback(Layer *me, GContext* ctx) {
-    (void)me;
-
-    PblTm time;
-    int i, iBarWidth, iBarHeight, iBarStart;
-    int iPointOne[2], iPointTwo[2], iPointThree[2], iPointFour;
+void bar_Calculations() {
     
     iBarStart=97;
     
     // Bar Dimensions
-    iBarHeight=iScreenHeight-(iScreenHeight-iBarStart);
-    iBarWidth=(iScreenWidth-20)/3;
+    iBarHeight=SCREEN_HEIGHT-(SCREEN_HEIGHT-iBarStart);
+    iBarWidth=(SCREEN_WIDTH-20)/3;
     
     // Bar Width Start
     iPointOne[0] = 5;
@@ -75,25 +37,56 @@ void bars_layer_update_callback(Layer *me, GContext* ctx) {
     iPointTwo[1] = iPointTwo[0] + iBarWidth;
     iPointThree[1] = iPointThree[0] + iBarWidth;
     
+}
+
+// Taken from Simplicity
+void handle_tick(AppContextRef ctx, PebbleTickEvent *t) {
+    (void)t;
+    (void)ctx;
+    
+    // Need to be static because they're used by the system later.
+    static char hour_text[] = "00";
+	static char min_text[] = "00";
+	static char sec_text[] = "00";
+    static char date_text[] = "Xxxxxxxxx 00";
+    
+    // time
+    string_format_time(hour_text, sizeof(hour_text), "%R", t->tick_time);
+	string_format_time(min_text, sizeof(min_text), "%M", t->tick_time);
+	string_format_time(sec_text, sizeof(sec_text), "%S", t->tick_time);
+    
+    // date
+    string_format_time(date_text, sizeof(date_text), "%B %e", t->tick_time);
+        
+    text_layer_set_text(&text_hour_layer, hour_text);
+	text_layer_set_text(&text_min_layer, min_text);
+	text_layer_set_text(&text_sec_layer, sec_text);
+	text_layer_set_text(&text_date_layer, date_text);
+    layer_mark_dirty(&bars_layer);
+}
+
+void bars_layer_update_callback(Layer *me, GContext* ctx) {
+    (void)me;
+    
     //Get the current time
-    get_time(&time);
+    get_time(&current_time);
     
     graphics_context_set_stroke_color(ctx, GColorWhite);
     
     // Hours
-    for (i=0; i<((time.tm_hour*iBarHeight)/24); i++) {
+    for (i=0; i<((current_time.tm_hour*iBarHeight)/24); i++) {
         iPointFour=iBarStart-(i);
         graphics_draw_line(ctx, GPoint(iPointOne[0], iPointFour), GPoint(iPointOne[1], iPointFour));
     }
     
     // Minutes
-    for (i=0; i<((time.tm_min*iBarHeight)/60); i++) {
+    for (i=0; i<((current_time.tm_min*iBarHeight)/60); i++) {
         iPointFour=iBarStart-(i);
         graphics_draw_line(ctx, GPoint(iPointTwo[0], iPointFour), GPoint(iPointTwo[1], iPointFour));
     }
     
     // Seconds
-    for (i=0; i<((time.tm_sec*iBarHeight)/60); i++) {
+    for (i=0; i<((current_time.tm_sec*iBarHeight)/60); i++) {
         iPointFour=iBarStart-(i);
         graphics_draw_line(ctx, GPoint(iPointThree[0], iPointFour), GPoint(iPointThree[1], iPointFour));
     }
@@ -108,23 +101,46 @@ void handle_init(AppContextRef ctx) {
     window_set_background_color(&window, GColorBlack);
     
     resource_init_current_app(&APP_RESOURCES);
-
+    
+    bar_Calculations();
     
     // bars
     layer_init(&bars_layer, window.layer.frame);
     bars_layer.update_proc = &bars_layer_update_callback;
     layer_add_child(&window.layer, &bars_layer);
     
-
     // time
-    text_layer_init(&text_time_layer, window.layer.frame);
-    text_layer_set_text_color(&text_time_layer, GColorWhite);
-    text_layer_set_background_color(&text_time_layer, GColorClear);
-    layer_set_frame(&text_time_layer.layer, GRect(7, 120, iScreenWidth-7, iScreenHeight-120));
-    text_layer_set_font(&text_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-    layer_add_child(&window.layer, &text_time_layer.layer);
-    
-   
+	// hour
+    text_layer_init(&text_hour_layer, window.layer.frame);
+    text_layer_set_text_color(&text_hour_layer, GColorWhite);
+    text_layer_set_background_color(&text_hour_layer, GColorClear);
+    layer_set_frame(&text_hour_layer.layer, GRect(iPointOne[0]+(iBarWidth/3)-2.5, 100, iPointOne[1], SCREEN_HEIGHT-100));
+    text_layer_set_font(&text_hour_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    layer_add_child(&window.layer, &text_hour_layer.layer);
+
+	// min
+	text_layer_init(&text_min_layer, window.layer.frame);
+    text_layer_set_text_color(&text_min_layer, GColorWhite);
+    text_layer_set_background_color(&text_min_layer, GColorClear);
+    layer_set_frame(&text_min_layer.layer, GRect(iPointTwo[0]+(iBarWidth/3)-2.5, 100, iPointTwo[1], SCREEN_HEIGHT-100));
+    text_layer_set_font(&text_min_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    layer_add_child(&window.layer, &text_min_layer.layer);
+
+	// sec
+	text_layer_init(&text_sec_layer, window.layer.frame);
+    text_layer_set_text_color(&text_sec_layer, GColorWhite);
+    text_layer_set_background_color(&text_sec_layer, GColorClear);
+    layer_set_frame(&text_sec_layer.layer, GRect(iPointThree[0]+(iBarWidth/3)-2.5, 100, iPointThree[1], SCREEN_HEIGHT-100));
+    text_layer_set_font(&text_sec_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    layer_add_child(&window.layer, &text_sec_layer.layer);
+
+	// date
+    text_layer_init(&text_date_layer, window.layer.frame);
+    text_layer_set_text_color(&text_date_layer, GColorWhite);
+    text_layer_set_background_color(&text_date_layer, GColorClear);
+    layer_set_frame(&text_date_layer.layer, GRect(iPointOne[0]+((iBarWidth/4)*3), 125, SCREEN_WIDTH-iPointOne[0]+(iBarWidth/2), SCREEN_HEIGHT-125));
+    text_layer_set_font(&text_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+    layer_add_child(&window.layer, &text_date_layer.layer);
     
 }
 
